a411569c7bc33e0a64dc62b681041cbf858be3ed
Michele <mahelkenn@gmail.com>
2021-05-02 00:25:32 -0800

    Added function to print guessed lettes on page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 /* -------------------------- Element Selection --------------------------------------- */
 /* Unordered list where the player's guessed letters will appear */
 const lettersList = document.querySelector(".guessed-letters");
+
 /*The button with the text "Guess!" in it */
 const guessButton = document.querySelector(".guess");
+
 /* The text input where the player will guess a letter */
 const textInput = document.querySelector(".letter");
+
 /* The empty paragraph where the word in progress will appear */
 const wordInProgress = document.querySelector(".word-in-progress");
+
 /* The paragraph where the remaining guesses will display */
 const remaining = document.querySelector(".remaining");
+
 /* The span inside the paragraph where the remaining guesses will display  */
 const span = document.querySelector("span");
+
 /* The empty paragraph where messages will appear when the player guesses a letter  */
 const message = document.querySelector(".message");
+
 /* The hidden button that will appear prompting the player to play again  */
 const newGameButton = document.querySelector(".play-again")
 
@@ -67,5 +74,15 @@ const makeGuess = function (letter) {
     else {
         guessedLetters.push(upper);
         console.log(guessedLetters);
+        displayLettersGuessed();
     }
 }
+
+const displayLettersGuessed = function () {
+    lettersList.innerHTML = "";
+    let li = document.createElement("li");
+    for (let letter of guessedLetters) {
+        li.innerText = letter;
+        lettersList.append(li)
+    }
+} 
